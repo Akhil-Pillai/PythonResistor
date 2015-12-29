fcc8571de61c2d48e7d2943c8644423eba985d79
Akhil Pillai <akhilpillai18@gmail.com>
2015-12-29 15:40:44 +0530

    Try Catch block added

--- Resistor.py
+++ Resistor.py
@@ -10,42 +10,48 @@
 #-------------------------------------------------------------------------------
 
 def fourband():
-    a= input("Enter colour of the first band on resistor : ")
-    val1=band(a)
-    val1=val1*10
+    try:
+        a= input("Enter colour of the first band on resistor : ")
+        val1=band(a)
+        val1=val1*10
 
-    b= input("Enter colour of the second band on resistor : ")
-    val2=band(b)
+        b= input("Enter colour of the second band on resistor : ")
+        val2=band(b)
 
-    mul= input("Enter colour of the third band on resistor : ")
-    val3=multiplier(mul)
+        mul= input("Enter colour of the third band on resistor : ")
+        val3=multiplier(mul)
 
-    tolerance= input("Enter colour of the fourth band on resistor : ")
-    val4=tol(tolerance)
-    finalresult= result(val1,val2,val3)
-    print(str(finalresult) +" ohms, " +str(val4))
-    return
+        tolerance= input("Enter colour of the fourth band on resistor : ")
+        val4=tol(tolerance)
+        finalresult= result(val1,val2,val3)
+        print(str(finalresult) +" ohms, " +str(val4))
+        return
+    except:
+        print("Invalid Input")
 
 def fiveband():
-    a= input("Enter colour of the first band on resistor : ")
-    val1=band(a)
-    val1=val1*100
+    try:
+        a= input("Enter colour of the first band on resistor : ")
+        val1=band(a)
+        val1=val1*100
 
-    b= input("Enter colour of the second band on resistor : ")
-    val2=band(b)
-    val2=val2*10
+        b= input("Enter colour of the second band on resistor : ")
+        val2=band(b)
+        val2=val2*10
 
-    c=input("Enter colour of the third band on resistor: ")
-    valc=band(c)
+        c=input("Enter colour of the third band on resistor: ")
+        valc=band(c)
 
-    mul= input("Enter colour of the fourth band on resistor : ")
-    val3=multiplier(mul)
+        mul= input("Enter colour of the fourth band on resistor : ")
+        val3=multiplier(mul)
 
-    tolerance= input("Enter colour of the fifth band on resistor : ")
-    val4=tol(tolerance)
-    finalresult= result(val1,val2,val3,valc)
-    print(str(finalresult) +" ohms, " +str(val4))
-    return
+        tolerance= input("Enter colour of the fifth band on resistor : ")
+        val4=tol(tolerance)
+        finalresult= result(val1,val2,val3,valc)
+        print(str(finalresult) +" ohms, " +str(val4))
+        return
+    except:
+        print('Invalid Input')
 
 def result(val1,val2,val3,valc=0):
     res = float ((val1 + val2 + valc) * val3)
@@ -61,8 +67,6 @@ def tol(strtol):
         	band='+/-5%'
         elif (strtol.lower() == 'silver'):
     	   	band='+/-10%'
-        else:
-            print('invalid colour')
         return band
 
 def multiplier(strmul):
@@ -84,35 +88,31 @@ def multiplier(strmul):
     	band=0.1
     elif (strmul.lower() == 'silver'):
 	   	band=0.01
-    else:
-        print('invlid colour')
     return band
 
 
 def band(strband):
-    if (strband.lower() == 'black'):
- 	   	band=0
-    elif (strband.lower() == 'brown'):
-        band=1
-    elif (strband.lower() == 'red'):
-	   	band=2
-    elif (strband.lower() == 'orange'):
-	   	band=3
-    elif (strband.lower() == 'yellow'):
-	   	band=4
-    elif (strband.lower() == 'green'):
-	   	band=5
-    elif (strband.lower() == 'blue'):
-	   	band=6
-    elif (strband.lower() == 'violet'):
-    	band=7
-    elif (strband.lower() == 'grey'):
-	   	band=8
-    elif (strband.lower() == 'white'):
-	   	band=9
-    else:
-	   	print("Invalid colour")
-    return band
+        if (strband.lower() == 'black'):
+     	   	band=0
+        elif (strband.lower() == 'brown'):
+            band=1
+        elif (strband.lower() == 'red'):
+    	   	band=2
+        elif (strband.lower() == 'orange'):
+    	   	band=3
+        elif (strband.lower() == 'yellow'):
+    	   	band=4
+        elif (strband.lower() == 'green'):
+    	   	band=5
+        elif (strband.lower() == 'blue'):
+    	   	band=6
+        elif (strband.lower() == 'violet'):
+        	band=7
+        elif (strband.lower() == 'grey'):
+    	   	band=8
+        elif (strband.lower() == 'white'):
+    	   	band=9
+        return band
 
 bandval = input("Enter the number of bands on the resistor : ")
 if bandval=='4':
